d3c88880fb093b9e17e07c11c41cf95a77e11220
ryan851109 <ryan851109@gmail.com>
2018-07-30 02:39:53 +0800

    modify code

--- version2/AES_function.py
+++ version2/AES_function.py
@@ -4,17 +4,13 @@ from binascii import b2a_hex,a2b_hex
 class AESCrypto():
 	def __init__(self,key):
 		self.key = key
-		self.mode = AES.MODE_CBC
-		#print(AES.block_size)
 
 	def encrypt(self,text):
 		if len(text)%16!=0:
 			text=text+str((16-len(text)%16)*'0')
-		cryptor = AES.new(self.key,self.mode,b'0000000000000000')
-		self.ciphertext = cryptor.encrypt(text)
-		return b2a_hex(self.ciphertext)
+		cryptor = AES.new(self.key,AES.MODE_CBC,b'0000000000000000')
+		return b2a_hex(cryptor.encrypt(text))
 
 	def decrypt(self,text):
-		cryptor = AES.new(self.key,self.mode,b'0000000000000000')
-		plain_text  = cryptor.decrypt(a2b_hex(text))
-		return plain_text.rstrip(b'\0')
+		cryptor = AES.new(self.key,AES.MODE_CBC,b'0000000000000000')
+		return (cryptor.decrypt(a2b_hex(text))).rstrip(b'\0')
